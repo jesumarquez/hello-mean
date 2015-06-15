d4351f0535f3d3926568485dcf8e335599f9dfb2
Jesu Marquez <jesumarquez@gmail.com>
2015-06-15 14:33:49 +0000

    Simple angulajs sample

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
 //listen
 app.listen(process.env.PORT, process.env.IP, null, function(){
     console.log('server is running... ');
