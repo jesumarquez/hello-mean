b80f6247640045f06d8c26cb5da3598480fc2134
Jesu Marquez <jesumarquez@gmail.com>
2015-06-15 17:44:04 +0000

    Controller customer list

--- public/scripts/customer/customer-controller.js
+++ public/scripts/customer/customer-controller.js
@@ -2,6 +2,17 @@ var app = angular.module('meanApp.controllers',[]);
 
 app.controller('customerController', ['$scope', 
     function($scope){
-        $scope.name = 'Customer1';
+
+        $scope.customers = [
+            { firstName : 'Jesú', lastName : 'Márquez' },
+            { firstName : 'Benjamín', lastName : 'Márquez' }
+            ];
+            
+        $scope.current = {};
+        
+        $scope.add = function(){
+            $scope.customers.push($scope.current);
+            $scope.current = {};
+        };
     }
 ]);
